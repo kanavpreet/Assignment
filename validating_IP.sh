e9de8a42899672464a28da84fbbc45ceb7b090ba
Kanav Preet <30400257+kanavpreet@users.noreply.github.com>
2020-01-08 15:11:06 +0530

    Update validating_IP.sh

--- validating_IP.sh
+++ validating_IP.sh
@@ -1,9 +1,11 @@
 #!bin/bash
 
-##Testing IP Validation 
 
-if [ $ip =~ ^[0-9] +\.[0-9]+\.[0-9] +\.[0-9]]; then
+read -p "Enter your IP address" ip
+
+if [ $ip =~ ^[0-9]{1-3} +\.[0-9]{1-3}+\.[0-9] +\.[0-9]{1-3}] then
+ //if  [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \ && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]] then
   echo "Success"
 else 
   echo "Fail"
- 
+ fi
