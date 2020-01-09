42ef8a7d874c100359b4aad7606faaf606f74ead
Kanav Preet <30400257+kanavpreet@users.noreply.github.com>
2020-01-09 12:58:13 +0530

    Validating IPV4 IP in shell script

--- validating_IP.sh
+++ validating_IP.sh
@@ -1,17 +1,21 @@
 #!bin/bash
 
 
-read -p "Enter your IP address" ip
-function check_ip(){
-if [ $ip =~ ^[0-9]{1-3} +\.[0-9]{1-3}+\.[0-9] +\.[0-9]{1-3}] 
-        #OIFS=$IFS
-        IFS='.'
-        ip=($ip)
-        #IFS=$OIFS
-     if [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \ && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]] then
-         echo "Success"
-  
+read -p "Enter your IP address   " ip
+
+if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]];
+then
+	OIFS=$IFS
+	IFS='.'
+	ip=($ip)
+	IFS=$OIFS
+	if [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]];
+	then
+		echo "Success"
+	else 
+		echo "IP can't be more than 255"
+	fi
+	
 else 
-  echo "Fail"
- fi
-}
+   echo "Fail -IP not there"
+fi 
